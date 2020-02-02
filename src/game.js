ce9fd5cdd648b867fc133b35fe0a227090bcd4b8
SeminVsevolod <semin.vsevolod@gmail.com>
2020-02-02 18:49:50 +0300

    add tests which writes user's symbol in top left cell and given coordinates

--- src/game.js
+++ src/game.js
@@ -1,9 +1,22 @@
 export default class Game {
-    getState() {
-        return [
+    constructor() {
+        this._userMoveSymbol = '×';
+        this._board = [
             ['', '', ''],
             ['', '', ''],
             ['', '', '']
-        ]
+        ];
+    }
+
+    getState() {
+        return this._board;
+    }
+
+    acceptUserMove(x, y) {
+        this._updateBoard(x, y);
+    }
+
+    _updateBoard(x, y) {
+        this._board[x][y] = this._userMoveSymbol;
     }
 }
--- test/game.js
+++ test/game.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
-
 import Game from '../src/Game';
 
+const userMoveSymbol = '×';
 const initialGameBoard = [
     ['', '', ''],
     ['', '', ''],
@@ -16,7 +16,20 @@ beforeEach(() => {
 describe('Game', () => {
     it('Should return empty game board', () => {
         const board = game.getState();
-
         expect(board).to.deep.equal(initialGameBoard);
+    });
+
+    it('Writes user\'s symbol in top left cell', () => {
+        const x = 0, y = 0;
+        game.acceptUserMove(x, y);
+        const board = game.getState();
+        expect(board[x][y]).to.equal(userMoveSymbol);
+    });
+
+    it('Writes user\'s symbol in cell with given coordinates', () => {
+        const x = 1, y = 1;
+        game.acceptUserMove(x, y);
+        const board = game.getState();
+        expect(board[x][y]).to.equal(userMoveSymbol)
     });
 });
