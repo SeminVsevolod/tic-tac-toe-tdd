4ea6860b7205210ab9880bfe9cd9855573d23be0
SeminVsevolod <semin.vsevolod@gmail.com>
2020-02-08 08:53:38 +0300

    testing saving user\'s move in history

--- src/game.js
+++ src/game.js
@@ -26,6 +26,10 @@ export default class Game {
         })
     }
 
+    getMoveHistory() {
+
+    }
+
     _updateBoard(x, y, config = {}) {
         const {symbol = this._userMoveSymbol} = config;
         this._board[x][y] = symbol;
