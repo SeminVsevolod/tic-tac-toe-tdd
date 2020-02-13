29b63b8822247ddac7af5374048321b082f7a007
SeminVsevolod <semin.vsevolod@gmail.com>
2020-02-13 13:50:59 +0300

    red testing creates table with 3 rows and 3 columns

--- test/dom.js
+++ test/dom.js
@@ -3,11 +3,15 @@ import jsdom from 'jsdom';
 import DomController from '../src/DomContoller';
 
 const {JSDOM} = jsdom;
-const dom = new JSDOM('<html><body id="root"></body></html>');
+const dom = new JSDOM('<html lang="en"><body id="root"></body></html>');
 const createInstance = () => new DomController('#root');
 
 global.window = dom.window;
 global.document = dom.window.document;
+
+afterEach(() => {
+    document.body.innerHTML = '';
+});
 
 describe('DOM controller', () => {
     it('Creates empty table', () => {
@@ -16,5 +20,15 @@ describe('DOM controller', () => {
         domController.createTable();
 
         expect(document.querySelectorAll('table').length).to.equal(1);
+    });
+
+    it('Creates table with 3 rows and 3 columns', () => {
+        const domController = createInstance();
+
+        domController.createTable(3, 3);
+
+        expect(document.querySelectorAll('table').length).to.equal(1);
+        expect(document.querySelectorAll('tr').length).to.equal(3);
+        expect(document.querySelectorAll('td').length).to.equal(9);
     });
 });
